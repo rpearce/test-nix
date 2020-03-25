b4847bc1efef6417d0070c81df1b6510d226cd43
Robert Pearce <me@robertwpearce.com>
2020-03-24 21:35:27 -0400

    refactor setup.sh

--- setup.sh
+++ setup.sh
@@ -6,9 +6,8 @@ done
 function link_bins {
   ln -fs $0 "$1/bin/$(basename $0)"
 }
-export -f link_bins
 
-function build {
+function build_bins {
   mkdir -p $out/bin
 
   for t in $baseInputs $buildInputs; do
@@ -18,15 +17,26 @@ function build {
       -maxdepth 1 \
       -exec /bin/bash -c 'link_bins $@ $out' {} \;
   done
+}
 
+function build_dotfiles {
   for d in $dotfiles; do
-    file_name=${d%=*}
+    file_path=${d%=*}
     store_path=${d#*=}
 
-    if [[ $file_name == *"/"* ]]; then
-      mkdir -p $(dirname "$out/$file_name")
+    if [[ $file_path == *"/"* ]]; then
+      mkdir -p $(dirname "$out/$file_path")
     fi
 
-    ln -fs $store_path "$out/$file_name"
+    ln -fs $store_path "$out/$file_path"
   done
+}
+
+export -f build_bins \
+          build_dotfiles \
+          link_bins
+
+function build {
+  build_bins
+  build_dotfiles
 }
