bebceb3f02155d11494ddf8cd54a75fa8b2372d3
Robert Pearce <me@robertwpearce.com>
2020-04-04 17:17:05 -0400

    edge nixpkgs; other stuffs

--- default.nix
+++ default.nix
@@ -1,10 +1,11 @@
+{ }:
+
 let
   sources = import ./nix/sources.nix;
-  pkgs = (import sources.nixpkgs) { config = { allowUnfree = true; }; };
+  pkgs = import sources.nixpkgs { config = { }; };
   mkDerivation = import sources.mgmt pkgs;
 in {
-  home = import ./mgmt-home.nix {
-    mkDerivation = mkDerivation;
-    pkgs = pkgs;
+  home = import ./home.nix {
+    inherit mkDerivation pkgs;
   };
 }
